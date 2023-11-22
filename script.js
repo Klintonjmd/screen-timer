fec7d17c7c4ce03c9eec4d09dc65c9550096996f
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2023-11-21 22:58:22 -0500

    new file:   buzzer.wav 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const passive = document.querySelector("#passive");
 const active = document.querySelector("#active");
+let i = 0;
+var buzzer = new Audio("buzzer.wav");
 
 window.onload = beginTimer();
 
 function beginTimer() {
+  document.title = "Sceen Timer";
   passive.style.display = "block";
   active.style.display = "none";
-  //setTimeout(beginBreak, 5000);
+  setTimeout(beginBreak, 1000);
 }
 
-// function beginBreak() {
-//   console.log("beginBreak");
-//   passive.style.display = "none";
-//   active.style.display = "block";
-//   setTimeout(beginTimer, 5000);
-// }
+function beginBreak() {
+  if (document.visibilityState === "hidden") {
+    buzzer.play();
+    document.title = "TIME FOR A BREAK";
+  }
+  if (document.visibilityState === "visible") {
+    passive.style.display = "none";
+    active.style.display = "block";
+    setTimeout(beginTimer, 1000);
+  }
+}
