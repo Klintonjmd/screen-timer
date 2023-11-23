b9f8af1e5871e0b7046368e59ce320f738c88f2b
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2023-11-22 21:22:40 -0500

    Complete basic functionality

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const passive = document.querySelector("#passive");
 const active = document.querySelector("#active");
-let i = 0;
-let j = 0;
 var buzzer = new Audio("buzzer.wav");
 
 window.onload = beginTimer();
@@ -10,23 +8,12 @@ function beginTimer() {
   document.title = "Sceen Timer";
   passive.style.display = "block";
   active.style.display = "none";
-  setTimeout(beginBreak, 1000);
+  setTimeout(beginBreak, 1200000);
 }
 
 function beginBreak() {
-  if (document.visibilityState === "hidden") {
-    buzzer.play();
-    document.title = "TIME FOR A BREAK";
-    //while loop breaks page
-    // while (j === 0) {
-    //   if (document.visibilityState === "visible") {
-    //     j = 1;
-    //   }
-    // }
-  }
-  if (document.visibilityState === "visible") {
-    passive.style.display = "none";
-    active.style.display = "block";
-    setTimeout(beginTimer, 1000);
-  }
+  buzzer.play();
+  passive.style.display = "none";
+  active.style.display = "block";
+  setTimeout(beginTimer, 90000);
 }
